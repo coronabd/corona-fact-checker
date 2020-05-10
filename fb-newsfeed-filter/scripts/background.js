ffc62d4c8c0f1401d4d97866ec8f682710dde55a
iamaureen <ishrat.ahmed@asu.edu>
2020-05-10 06:17:53 -0400

    custom block msg added

--- fb-newsfeed-filter/scripts/background.js
+++ fb-newsfeed-filter/scripts/background.js
@@ -5,14 +5,28 @@ var pattern = ["https://www.facebook.com/lalsalu.page/posts/2769601203135709",
               "https://www.facebook.com/mh.mon.94/videos/638619193593343/",
               "https://*.bing.com/*"];
 
+var requestedURL;
 // cancel function returns an object
 // which contains a property `cancel` set to `true`
 function cancel(requestDetails) {
   //TODO: handle www or w/o www
   console.log("Canceling: " + requestDetails.url);
-  return {cancel: true};
+  //display custom popup message
+  //chrome.tabs.update({url: "https://example.com"});
+  requestedURL = requestDetails.url;
+  return {redirectUrl: chrome.extension.getURL("templates/blockmsg.html") };
+
+  //return {cancel: true};
 }
 
 // add the listener,
 // passing the filter argument and "blocking"
-chrome.webRequest.onBeforeRequest.addListener(cancel, {urls: pattern},["blocking"]);
+chrome.webRequest.onBeforeRequest.addListener(cancel, {urls: pattern}, ["blocking"]);
+
+//TODO: not working yet
+$('#okbutton').click(function()
+{
+  alert("this button was clicked");
+  //alert(chrome.extension.getBackgroundPage().URLStorage);
+  //window.location.href = chrome.extension.getBackgroundPage().URLStorage;
+});
