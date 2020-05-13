48ad0c26a6063ad8ac44b65c64059fe731d6696c
nusratsharmin03 <36680551+nusratsharmin03@users.noreply.github.com>
2020-05-13 14:33:56 -0400

    Update background.js

--- fb-newsfeed-filter/scripts/background.js
+++ fb-newsfeed-filter/scripts/background.js
@@ -3,9 +3,10 @@
 var pattern = ["https://www.facebook.com/lalsalu.page/posts/2769601203135709",
               "https://www.facebook.com/groups/brahmanbarian2017/permalink/3068719746521623/",
               "https://www.facebook.com/mh.mon.94/videos/638619193593343/",
+              "https://*.bing.com/*",
+              "https://www.facebook.com/mh.mon.94/*",
               "https://*.bing.com/*"];
-
-
+var currenturl = ""
 
 //post request to server
 var serverData;
@@ -28,8 +29,6 @@ xmlhttp.onreadystatechange = function() {
 xmlhttp.open("POST", url, false);
 xmlhttp.send();
 
-
-
 var requestedURL;
 // cancel function returns an object
 // which contains a property `cancel` set to `true`
@@ -41,20 +40,26 @@ function cancel(requestDetails) {
   //display custom popup message
   //chrome.tabs.update({url: "https://example.com"});
   requestedURL = requestDetails.url;
-  return {redirectUrl: chrome.extension.getURL("templates/blockmsg.html") };
 
-  //return {cancel: true};
+  // return {redirectUrl: chrome.extension.getURL("templates/blockmsg.html") };
+  return {redirectUrl: chrome.extension.getURL("templates/blockmsg.html"+"?&from="+currenturl+"&blocked="+requestedURL)};
+
+  return {cancel: true};
 }
 
 
 // add the listener,
-// passing the filter argument and "blocking"
+// passing the filter argument and "blocking" 
 chrome.webRequest.onBeforeRequest.addListener(cancel, {urls: urlList}, ["blocking"]);
 
-//TODO: not working yet
-$('#okbutton').click(function()
-{
-  alert("this button was clicked");
-  //alert(chrome.extension.getBackgroundPage().URLStorage);
-  //window.location.href = chrome.extension.getBackgroundPage().URLStorage;
-});
+// listen to tab changes event and page loading complete event
+// capture the current url, then pass onto the blockmsg.html for "go back" button
+function temp(req){
+  chrome.tabs.query({active: true, lastFocusedWindow: true, currentWindow: true}, function (tabs) {
+    console.log("CURRENT TAB "+tabs[0].url);
+    currenturl = tabs[0].url;
+  });
+}
+
+chrome.webRequest.onCompleted.addListener(temp,{urls: ["<all_urls>"]});
+chrome.tabs.onActivated.addListener(temp)
