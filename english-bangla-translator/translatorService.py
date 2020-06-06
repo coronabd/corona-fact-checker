0f15f0807d3818eefd0fb375b3114884a32177ca
Rahul <vaish.rahul4810@gmail.com>
2020-06-06 08:00:52 +0530

    english bangla translation service

--- english-bangla-translator/translatorService.py
+++ english-bangla-translator/translatorService.py
@@ -15,14 +15,14 @@ def api_welcome():
 def api_englishToBangla():
     content = request.get_json()
     translatedText = str(englishToBangla.translate(content['sourceText']))
-    return jsonify(translatedText)
+    return jsonify(outputText=translatedText)
 
 
 @app.route('/bntoen', methods = ['POST'])
 def api_banglaToEnglish():
     content = request.get_json()
     translatedText = str(banglaToEnglish.translate(content['sourceText']))
-    return jsonify(translatedText)
+    return jsonify(outputText=translatedText)
  
 if __name__ == '__main__':
     app.run(debug=True)#,host='0.0.0.0')
